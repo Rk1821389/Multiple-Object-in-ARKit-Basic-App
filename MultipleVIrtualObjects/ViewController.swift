7bc5f4b6ec9c06552d853b39b82daa90e014b1ed
Rahul <rk1821389@gmail.com>
2023-02-13 09:38:01 +0530

    Added multiple object and added tap gesture

--- MultipleVIrtualObjects/ViewController.swift
+++ MultipleVIrtualObjects/ViewController.swift
@@ -23,10 +23,44 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
         
         // Set the scene to the view
         sceneView.scene = scene
+        
+        let box = SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0)
+        let material = SCNMaterial()
+        material.name = "Color"
+        material.diffuse.contents = UIImage(named: "wood")
+        
+        let boxNode = SCNNode(geometry: box)
+        boxNode.position = SCNVector3(x: 0, y: 0, z: -0.5)
+        boxNode.geometry?.materials = [material]
+        
+        let sphere = SCNSphere(radius: 0.3)
+        sphere.firstMaterial?.diffuse.contents = UIImage(named: "earth")
+        
+        let sphereNode = SCNNode(geometry: sphere)
+        sphereNode.position = SCNVector3(x: 0.5, y: 0, z: -0.5)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(taped))
+        self.sceneView.addGestureRecognizer(tapGesture)
+        
+        self.sceneView.scene.rootNode.addChildNode(sphereNode)
+        self.sceneView.scene.rootNode.addChildNode(boxNode)
+    }
+    
+    @objc func taped(recognizer: UITapGestureRecognizer) {
+        let sceneView = recognizer.view as! SCNView
+        let touchLocation = recognizer.location(in: sceneView)
+        let hitResults = sceneView.hitTest(touchLocation, options: [:])
+        
+        if !hitResults.isEmpty {
+            let node = hitResults[0].node
+            let material = node.geometry?.material(named: "Color")
+            
+            material?.diffuse.contents = UIColor.red
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -44,31 +78,5 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
-    }
-
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
     }
 }
